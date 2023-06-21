994f90048a07a7ef3263f51cf5fe27c5ca4874d9
Blurry Face <kakashi.code.take@gmail.com>
2023-06-21 10:49:49 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
+import { NavBar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'
 
 const App = () => (
     <BrowserRouter>
       <Box sx={{backgroundColor:'#000'}}>
         <NavBar />
-        <Route>
+        <Routes>
           <Route path="/" exact element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetail />} />
           <Route path="/channel/:id" element={<ChannelDetail />} />
           <Route path="/search/:searchTerm" element={<SearchFeed />} />
-
-
-        </Route>
+        </Routes>
       </Box>
     </BrowserRouter>
   )
